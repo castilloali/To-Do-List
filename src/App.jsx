24e6a4c06560e394bccd3f29e2c63e54d704dc26
cass <alicastilo1097@gmail.com>
2023-07-09 09:30:19 +0300

    create react

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+
+
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+
+
+  return (
+    <div>
+      <div className="container">
+        <div>
+          <div class="input-group">
+            <input required="" type="text" name="text" autocomplete="off" class="input mt-5"/>
+            <label class="user-label">First Name</label>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
