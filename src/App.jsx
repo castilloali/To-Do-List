33411a0cb52b110ad6162bcacdb0d54b798d6cba
cass <alicastilo1097@gmail.com>
2023-07-11 08:49:44 +0300

    create style

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,20 @@
 
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
+import { Button } from "react-bootstrap"
+
 function App() {
 
 
   return (
     <div>
       <div className="container">
-        <div>
-          <div class="input-group">
-            <input required="" type="text" name="text" autocomplete="off" class="input mt-5"/>
-            <label class="user-label">First Name</label>
+        <div className="hold">
+          <div class=" position-relative">
+            <label className="position-absolute " htmlFor="item">New Item</label>
+            <input className="position-absolute w-100" type="text"  class="input"/>
           </div>
+          <Button className="button">Add</Button>
         </div>
       </div>
     </div>
